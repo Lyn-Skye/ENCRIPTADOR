28bd9fdc9e69d966dd980a74dec0f5e0536a0699
Hilda Lacaze <qfbhildalacaze@gmail.com>
2024-02-21 22:39:15 -0500

    1-Agregué el archivo Vector.png 2-Agregué la linea de letras solo mayúscula y minúscula 3-Modifiqué el tipo de botones de input type submit a button 4-Modifiqué todas los valores de dimensiones a porcentajes 5-Saqué el texto e imagen de muñeco de la clase recuadro 6-Modifiqué los valores originales del Figma a los que me gustaron más visualmente 7-Cree el archivo Encriptador.js 8-Referencié el js en el html 9-Tomé códigos de ejemplo y los puse como comentarios en el js

---
+++ Encriptador.js
@@ -0,0 +1,30 @@
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+/*
+var textarea = document.getElementById("my-button");
+button.addEventListener("click", changeColor);
+
+function changeColor() {
+  document.getElementById("my-div").style.backgroundColor = "green";
+}
+
+
+
+function swap(str) {
+    var texto = ''
+    var string;
+    for (i = 0; i <= str.length - 1; i++) {
+      string = str[i];  
+      if (str[i] == string.toUpperCase()) {
+        texto += string.toLowerCase();
+      } else {
+        texto += string.toUpperCase();
+      }
+    }
+    return texto;
+  }
+  
+  console.log(swap('PeTeR')); */
